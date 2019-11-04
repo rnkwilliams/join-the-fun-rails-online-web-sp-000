0680e12313096cfcdd3d353a0606621303ae3239
Ranika Williams <rnkprice07@gmail.com>
2019-11-04 21:21:27 +0000

    Automatically backed up by Learn

--- db/migrate/20191104204412_create_join_table_taxis_rides_passengers.rb
+++
@@ -1,9 +0,0 @@
-class CreateJoinTableTaxisRidesPassengers < ActiveRecord::Migration[5.0]
-  def change
-    create_table :taxis_rides_passengers do |t|
-      t.integer :taxi_id
-      t.integer :ride_id
-      t.integer :passenger_id
-    end
-  end
-end
